472250f46621dc365f5be69073e3d3e6c551e222
Jean Crus <jean.carlos@pucpr.edu.br>
2018-12-06 13:53:22 -0200

    Projeto criado

---
+++ src/course/SquareRoots.java
@@ -0,0 +1,17 @@
+package course;
+
+public class SquareRoots {
+	public static double sqrt(int number) {
+		double t;
+	 
+		double squareRoot = number / 2;
+	 
+		do {
+			t = squareRoot;
+			squareRoot = (t + (number / t)) / 2;
+		} while ((t - squareRoot) != 0);
+	 
+		return squareRoot;
+	}
+}
+	
